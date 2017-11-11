02ba872b1a649837cc36428898231b61b7dbb15a
Nikos Korfiatis <nikos.korfiatis@gmail.com>
2017-11-11 22:44:47 +0000

    update

---
+++ js/customheader.js
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create','UA-38613273-1', 'auto');
+ga('send', 'pageview');
+
+
+/**
+* Function that tracks a click on an outbound link in Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label. Setting the transport method to 'beacon' lets the hit be sent
+* using 'navigator.sendBeacon' in browser that support it.
+*/
+var trackOutboundLink = function(url,type) {
+   ga('send', 'event', type , 'click', url, {
+     'transport': 'beacon',
+     'hitCallback': function(){
+        window.open(url,'_blank');
+     }
+   });
+}
+
+</script>
